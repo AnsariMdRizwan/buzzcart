f16180c69a106dcf714dab41fd319eda5fdd761e
M.D Rizwan <136155851+AnsariMdRizwan@users.noreply.github.com>
2025-02-21 20:27:46 +0530

    Update orders-controller.js

--- backend/controllers/shop/orders-controller.js
+++ backend/controllers/shop/orders-controller.js
@@ -18,8 +18,8 @@ const createOrder = async (req, res) => {
                 payment_method: 'paypal',
             },
             redirect_urls: {
-                return_url: 'https://buzzcarttt.netlify.app//shop/paypal-return',
-                cancel_url: 'https://buzzcarttt.netlify.app//shop/paypal-cancel',
+                return_url: 'https://buzzcarttt.netlify.app/shop/paypal-return',
+                cancel_url: 'https://buzzcarttt.netlify.app/shop/paypal-cancel',
             },
             transactions: [
                 {
