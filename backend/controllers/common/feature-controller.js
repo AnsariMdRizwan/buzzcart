a51c862dcdac570b1b479190e1cb088fde3ffcf3
Rizwan <arizwan9002@gmail.com>
2025-02-21 19:41:41 +0530

    update the backend file

--- backend/controllers/common/feature-controller.js
+++ backend/controllers/common/feature-controller.js
@@ -14,7 +14,6 @@ const addFeatureImage = async (req, res) => {
             data: featureImage
         })
 
-
     } catch (e) {
         res.status(500).json({
             success: false,
--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 const MONGO_URI = process.env.MONGO_URI; // Use MongoDB connection from .env
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+const CLIENT_URL = "https://buzzcarttt.netlify.app";
 
 app.use(
     cors({
